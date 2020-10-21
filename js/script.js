e7e30a4a9338d8e0f2fdbe1e037a675705adb074
yoneshinzato <chibariyo@gmail.com>
2020-10-20 23:33:22 -0300

    aplicando owl carousel com jQuery e responsividade

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
+        },
+        1024:{
+            items:5
+        }
+    }
+})
